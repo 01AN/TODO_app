80925e4bd15a569118e0fae3bd3f93755339beef
ESPORTS-141\cre <t-shirai@oca.ac.jp>
2022-11-08 19:20:55 +0900

    修正 1108

--- storage/framework/views/bde4943e3226e9417be79e3fb3b9dbb210210760.php
+++ storage/framework/views/bde4943e3226e9417be79e3fb3b9dbb210210760.php
@@ -30,9 +30,6 @@
                               <th scope="col"
                                   class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">
                                   完了済みタスク</th>
-                              <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                  <span class="sr-only">Actions</span>
-                              </th>
                           </tr>
                       </thead>
                       <tbody class="divide-y divide-gray-200 bg-white">
